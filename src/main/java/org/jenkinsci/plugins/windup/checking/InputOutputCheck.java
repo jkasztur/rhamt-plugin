2cd621f311d513b96dbebde393e82709781d2e13
Jan Kasztura <jkasztur@redhat.com>
2017-07-22 19:25:22 +0200

    [closes #5] Add input and output options

---
+++ src/main/java/org/jenkinsci/plugins/windup/checking/InputOutputCheck.java
@@ -0,0 +1,35 @@
+package org.jenkinsci.plugins.windup.checking;
+
+import java.io.File;
+
+import hudson.util.FormValidation;
+
+public class InputOutputCheck {
+
+	public static FormValidation checkInput(String value) {
+		if (value == null || value.trim().equals(""))
+			return FormValidation.error("No input specified");
+
+		String[] inputs = value.split(",");
+		for (String input : inputs) {
+			final File inputFile = new File(input);
+			if (!inputFile.exists()) {
+				return FormValidation.warning(input + " does not exist.");
+			}
+		}
+
+		return FormValidation.ok();
+	}
+
+	public static FormValidation checkOutput(String value) {
+		if (value == null || value.trim().equals(""))
+			return FormValidation.warning("No output directory specified");
+
+		final File output = new File(value);
+		if (output.exists()) {
+			return FormValidation.warning("Output directory will be overwritten.");
+		}
+
+		return FormValidation.ok();
+	}
+}
